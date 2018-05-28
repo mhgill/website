3249f6661e1ab35873e3c141b2961b33ae341dd6
Muhammad Gill <mugill@blackberry.com>
2018-05-28 14:32:33 -0400

    o

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var mail = require('./mail')
 var app = express();
 var bodyParser = require('body-parser')
 
+app.use(express.static(__dirname + "/public"));
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -12,8 +14,6 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
-
-app.use(express.static(__dirname + "/public"));
 
 app.use(favicon(__dirname + '/favicon.ico'));
 
